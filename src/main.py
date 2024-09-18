a4f01c11500e328d75ab7b43ff17b58b6d1ed894
SlotyHolly <matiasmusso3032@gmail.com>
2024-09-18 03:47:58 -0300

    Creacion de carpeta Funciones
    Creacion de carpeta Funciones con sus respectivos archivos

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# main.py
+
+from ui.login import window  # Importando la ventana desde login.py
+
+def main():
+    # Inicia la aplicación llamando a la ventana de login
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
