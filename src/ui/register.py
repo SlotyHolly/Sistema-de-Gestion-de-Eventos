f72df35477a88155e9deacd24665ad675449c67f
SlotyHolly <matiasmusso3032@gmail.com>
2024-09-18 02:42:59 -0300

    FIX Update UI Error

--- src/ui/register.py
+++ src/ui/register.py
@@ -11,7 +11,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"..\assets\register")
+ASSETS_PATH = OUTPUT_PATH / Path(r".\assets\register")
 
 
 def relative_to_assets(path: str) -> Path:
